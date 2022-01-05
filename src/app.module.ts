4763b55c7f37e06256bbf95a80cba1d54493c52f
Alexander <Alexander_alatorre1993@hotmail.com>
2022-01-04 19:50:04 -0800

    added postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule {
+
+}
